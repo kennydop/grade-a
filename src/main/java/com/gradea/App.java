aac66e4baa631581c4ba0f72be955e17df07c035
$inevitable55GH <dansooffeipatrick@gmail.com>
2023-08-10 03:56:26 +0000

    splash screen

--- src/main/java/com/gradea/App.java
+++ src/main/java/com/gradea/App.java
@@ -1,10 +1,13 @@
 package com.gradea;
 
+import javafx.animation.PauseTransition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
 
 import java.io.IOException;
 
@@ -21,25 +24,61 @@ public class App extends Application {
   }
 
   private static Scene scene;
+  private static Scene splashScene;
 
   @Override
   public void start(Stage stage) throws IOException {
-    Session.getInstance().loadUserFromPrefs();
-    Session userSession = Session.getInstance();
+    // Load and show splash screen first
+    Stage splashStage = new Stage();
 
-    String fxml;
-    if (userSession.getCurrentUser() != null) {
-      fxml = "dashboard";
-    } else {
-      fxml = "auth";
-    }
+    splashScene = new Scene(loadFXML("splash"));
+    splashScene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+    splashStage.setScene(splashScene);
+    splashStage.initStyle(StageStyle.UNDECORATED);
+    splashStage.show();
+    PauseTransition pause = new PauseTransition(Duration.seconds(3));
 
-    scene = new Scene(loadFXML(fxml), 1000, 600);
-    scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
-    stage.setScene(scene);
-    stage.setTitle("Grade A");
-    // stage.setMaximized(true);
-    stage.show();
+    pause.setOnFinished(e -> {
+
+      Session.getInstance().loadUserFromPrefs();
+      Session userSession = Session.getInstance();
+
+      String fxml;
+      if (userSession.getCurrentUser() != null) {
+        fxml = "dashboard";
+      } else {
+        fxml = "auth";
+      }
+
+      try {
+        scene = new Scene(loadFXML(fxml), 1000, 600);
+        scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+        stage.setScene(scene);
+        stage.setTitle("Grade A");
+        splashStage.close();
+        stage.show();
+      } catch (IOException ioException) {
+        ioException.printStackTrace();
+      }
+    });
+    pause.play();
+
+    // Session.getInstance().loadUserFromPrefs();
+    // Session userSession = Session.getInstance();
+
+    // String fxml;
+    // if (userSession.getCurrentUser() != null) {
+    // fxml = "dashboard";
+    // } else {
+    // fxml = "auth";
+    // }
+
+    // scene = new Scene(loadFXML(fxml), 1000, 600);
+    // scene.getStylesheets().add(getClass().getResource("style.css").toExternalForm());
+    // stage.setScene(scene);
+    // stage.setTitle("Grade A");
+    // // stage.setMaximized(true);
+    // stage.show();
   }
 
   static void setRoot(String fxml) throws IOException {
